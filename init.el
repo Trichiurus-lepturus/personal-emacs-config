519a3a87d101e2570771e98402731b92f787ea3a
Trichiurus-lepturus <144453663+Trichiurus-lepturus@users.noreply.github.com>
2026-02-06 13:12:32 +0800

    markdown

--- init.el
+++ init.el
@@ -268,6 +268,28 @@
         TeX-master nil)
   (add-hook 'TeX-mode-hook #'(lambda () (setq fill-column 78))))
 
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init
+  (setq markdown-command
+        '("pandoc" "--from=markdown" "--to=html5"
+          "--mathjax" "--standalone"))
+  :bind (:map markdown-mode-map ("C-c C-e" . markdown-do))
+  :custom
+  (markdown-open-command "firefox")
+  (markdown-fontify-code-blocks-natively t)
+  (markdown-enable-math t))
+
+(use-package markdown-toc
+  :ensure t
+  :custom
+  (markdown-toc-header-toc-start "<!-- toc-begin -->")
+  (markdown-toc-header-toc-title "")
+  (markdown-toc-header-toc-end "<!-- toc-end -->")
+  (markdown-toc-preset 'pandoc))
+
 ;; Dashboard
 (defvar-local sztk-dashboard--was-visible nil)
 
