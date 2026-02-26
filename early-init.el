bd7dbdfb30fb72289e5ec23810cb0a83b40912f2
Trichiurus-lepturus <144453663+Trichiurus-lepturus@users.noreply.github.com>
2026-02-26 16:24:14 +0800

    when not daemon, make frame visible after window setup

--- early-init.el
+++ early-init.el
@@ -1,6 +1,13 @@
+;;; early-init.el --- Early Init -*- lexical-binding: t; -*-
+
 (setq-default frame-background-mode 'dark)
 (setq frame-inhibit-implied-resize t)
 (add-to-list 'default-frame-alist '(width . 100))
 (add-to-list 'default-frame-alist '(height . 50))
 (add-to-list 'default-frame-alist '(font . "Fira Mono-12"))
+(unless (daemonp)
+  (add-to-list 'default-frame-alist '(visibility . nil))
+  (add-hook 'window-setup-hook (lambda () (make-frame-visible))))
 (setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
