192614d4067cd0673f16846cfb94d2ac401873a8
Trichiurus-lepturus <144453663+Trichiurus-lepturus@users.noreply.github.com>
2026-03-01 16:19:14 +0800

    fix wt launch and refactor function

--- lisp/init-terminal.el
+++ lisp/init-terminal.el
@@ -13,24 +13,38 @@
                       (format "cygpath -w \"%s\"" p)))
       (convert-standard-filename p))))
 
-(defun sztk-terminal--run (dir)
-  (let* ((dir (expand-file-name dir))
-         (default-directory dir)
-         (terminals
-          `(("konsole" "--new-tab" "--workdir" ,dir)
-            ("wt.exe" "-w" "0" "nt"
-             "-p" "MSYS2-UCRT64"))))
-    (unless (and (display-graphic-p)
-                 (not (file-remote-p dir))
-                 (cl-loop for (exe . args) in terminals
-                          when (executable-find exe)
-                          return (make-process
-                                  :name (concat exe "-from-emacs")
-                                  :buffer nil
-                                  :command (cons exe args)
-                                  :connection-type 'pipe
-                                  :noquery t)))
-      (vterm))))
+(defun sztk-terminal--run (directory)
+  (cl-flet ((external-p (dir)
+              (and (display-graphic-p) (not (file-remote-p dir))))
+            (konsole-p () (executable-find "konsole"))
+            (wt-p () (executable-find "wt.exe"))
+            (wt-nt-p ()
+              (let* ((ps-script "Get-Process WindowsTerminal \
+-ErrorAction SilentlyContinue | Where-Object {$_.MainWindowHandle -ne 0}")
+                     (cmd (concat "powershell.exe -NoProfile \
+-NonInteractive -Command " (shell-quote-argument ps-script)))
+                     (output (shell-command-to-string cmd)))
+                (not (string-empty-p (string-trim output)))))
+            (expand (dir)
+              (let ((dir (expand-file-name dir)))
+                (if windows-p (sztk-path-msys2-to-windows dir) dir))))
+    (let ((exterminal
+           (and (external-p directory)
+                (cond
+                 ((konsole-p)
+                  `("konsole" "--new-tab" "--workdir" ,(expand directory)))
+                 ((wt-p)
+                  `("cmd.exe" "/c" "start" "" "wt.exe"
+                    ,@(when (wt-nt-p) '("-w" "0" "nt"))
+                    "-d" ,(expand directory) "-p" "MSYS2-UCRT64"))
+                 (t nil)))))
+      (if exterminal
+          (make-process :name (car exterminal)
+                        :buffer nil
+                        :command exterminal
+                        :connection-type 'pipe
+                        :noquery t)
+        (vterm)))))
 
 (defun sztk-terminal-open-here ()
   (interactive)
